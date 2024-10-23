63173af01748ec5c52e6341482f1255f2b4da5bb
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-10-23 15:06:20 +0700

    Add IManufacturerService

---
+++ src/main/java/ITProject/example/WebSelling/service/intefaces/IManufacturerService.java
@@ -0,0 +1,19 @@
+package ITProject.example.WebSelling.service.intefaces;
+
+import ITProject.example.WebSelling.dto.request.ManufacturerRequest;
+import ITProject.example.WebSelling.entity.Manufacturer;
+
+import java.util.List;
+
+public interface IManufacturerService {
+    Manufacturer addManufacturer(ManufacturerRequest manufacturerRequest);
+
+    Manufacturer getManufacturerById(Long id);
+
+    List<Manufacturer> getAllManufacturers();
+
+    Manufacturer updateManufacturer(ManufacturerRequest manufacturerRequest, String name);
+
+    void deleteManufacturer(Long id);
+
+}
