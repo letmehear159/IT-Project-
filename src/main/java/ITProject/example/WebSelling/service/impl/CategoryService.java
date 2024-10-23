a6f6259ba1ddf6b690457df6e5aa5b0d0ba30903
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-10-23 15:04:41 +0700

    Add CategoryService

---
+++ src/main/java/ITProject/example/WebSelling/service/impl/CategoryService.java
@@ -0,0 +1,57 @@
+package ITProject.example.WebSelling.service.impl;
+
+import ITProject.example.WebSelling.dto.request.CategoryRequest;
+import ITProject.example.WebSelling.entity.Category;
+import ITProject.example.WebSelling.entity.Manufacturer;
+import ITProject.example.WebSelling.exceptionHandler.AppException;
+import ITProject.example.WebSelling.exceptionHandler.ErrorCode;
+import ITProject.example.WebSelling.mapper.CategoryMapper;
+import ITProject.example.WebSelling.repository.CategoryRepository;
+import ITProject.example.WebSelling.service.intefaces.ICategoryService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+public class CategoryService implements ICategoryService {
+
+    CategoryRepository categoryRepository;
+
+    CategoryMapper categoryMapper;
+
+    @Override
+    public Category addCategory(CategoryRequest categoryRequest) {
+        return (categoryRepository.save(categoryMapper.toCategory(categoryRequest)));
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id).orElseThrow(() -> new AppException(ErrorCode.INVALID_ID));
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category updateCategory(CategoryRequest categoryRequest, String name) {
+        Category category = categoryRepository
+                .findByCategoryName(name)
+                .orElseThrow(() -> new AppException(ErrorCode.INVALID_NAME));
+
+        categoryMapper.updateCategoryFromDTO(categoryRequest, category);
+
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
