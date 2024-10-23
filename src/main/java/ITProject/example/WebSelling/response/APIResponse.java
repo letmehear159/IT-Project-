a30f79c68f70be6f6d39dc7673f32f7365931ca7
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-10-23 15:03:16 +0700

    Add ApiResponse used for Exception Response

---
+++ src/main/java/ITProject/example/WebSelling/response/APIResponse.java
@@ -0,0 +1,20 @@
+package ITProject.example.WebSelling.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class APIResponse<T> {
+
+    int code = 1000;
+
+    String message;
+
+    T result;
+}
