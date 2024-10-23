d111659d7ea9121aaada420ed06bbf4b0f32f95f
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-10-23 15:05:09 +0700

    Add ErrorCode used in exception

---
+++ src/main/java/ITProject/example/WebSelling/exceptionHandler/ErrorCode.java
@@ -0,0 +1,34 @@
+package ITProject.example.WebSelling.exceptionHandler;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    USER_EXIST(1001, "User already exists", HttpStatus.BAD_REQUEST),
+    UNCATEGORIZED(9999, "Uncategorized", HttpStatus.SERVICE_UNAVAILABLE),
+    USERNAME_INVALID(1002, "Username's length must be greater or equal 5 characters", HttpStatus.BAD_REQUEST),
+    INVALID_EMAIL(1003, "Invalid email address", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004, "Password's length must be greater or equal 8 characters", HttpStatus.BAD_REQUEST),
+    INVALID_KEY(1005, "Can not find appropriate key", HttpStatus.NOT_FOUND),
+    USER_NON_EXISTENT(1006, "User does not exist", HttpStatus.NOT_FOUND),
+    USERNAME_PASSWORD_INVALID(1007, "User or password is incorrect", HttpStatus.UNAUTHORIZED),
+    UNAUTHENTICATED(1008, "Unauthenticated", HttpStatus.UNAUTHORIZED),
+    UNAUTHORIZED(1009, "Unauthorized", HttpStatus.FORBIDDEN),
+    INVALID_USERID(1010, "Character is not allowed is User ID", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1011, "Invalid date of birth, age must be at least {min}", HttpStatus.BAD_REQUEST),
+    VALUE_NOT_FOUND(1012, "Value does not exist", HttpStatus.NOT_FOUND),
+    INVALID_NAME(1013, "Name is invalid", HttpStatus.BAD_REQUEST),
+    INVALID_ID(1014, "Id is invalid", HttpStatus.BAD_REQUEST),
+    ;
+
+    private int code;
+    private String message;
+    private HttpStatus httpStatus;
+
+    ErrorCode(int code, String message, HttpStatus httpStatus) {
+        this.code = code;
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+}
