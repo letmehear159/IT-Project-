41f7c7db2ff4878fc7c9533e837e607566104aa6
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-10-23 15:08:11 +0700

    Update ManufacturerController, add CRUD endpoints

--- src/main/java/ITProject/example/WebSelling/restController/ManufacturerController.java
+++ src/main/java/ITProject/example/WebSelling/restController/ManufacturerController.java
@@ -1,25 +1,32 @@
 package ITProject.example.WebSelling.restController;
 
+import ITProject.example.WebSelling.dto.request.ManufacturerRequest;
 import ITProject.example.WebSelling.entity.Manufacturer;
+import ITProject.example.WebSelling.service.impl.ManufacturerService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/manufacturers")
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class ManufacturerController {
+
+    ManufacturerService manufacturerService;
+
     @PostMapping("/")
-    public ResponseEntity<?> addManufacturer(@RequestBody Object newManufacturer) {
-        return ResponseEntity.ok().body(newManufacturer);
+    public ResponseEntity<Manufacturer> addManufacturer(@RequestBody ManufacturerRequest manufacturerRequest) {
+        return ResponseEntity.ok().body(manufacturerService.addManufacturer(manufacturerRequest));
     }
 
     @GetMapping("/")
-    public ResponseEntity<?> getAllManufacturers() {
-        return ResponseEntity.ok().body(new Manufacturer());
+    public ResponseEntity<List<Manufacturer>> getAllManufacturers() {
+        return ResponseEntity.ok().body(manufacturerService.getAllManufacturers());
     }
 
     @GetMapping("/{id}")
@@ -27,5 +34,15 @@ public class ManufacturerController {
         return ResponseEntity.ok().body(new Manufacturer());
     }
 
+    @PutMapping("/manufacturer/{name}")
+    public ResponseEntity<Manufacturer> updateManufacturer(@PathVariable String name,
+                                                           @RequestBody ManufacturerRequest manufacturerRequest) {
+        return ResponseEntity.ok(manufacturerService.updateManufacturer(manufacturerRequest, name));
 
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteManufacturer(@PathVariable Long id) {
+        manufacturerService.deleteManufacturer(id);
+    }
 }
