126a453e8d2e1ba42ff24131df264890c6764788
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-10-23 15:05:52 +0700

    Add IcategoryService

---
+++ src/main/java/ITProject/example/WebSelling/service/intefaces/ICategoryService.java
@@ -0,0 +1,21 @@
+package ITProject.example.WebSelling.service.intefaces;
+
+import ITProject.example.WebSelling.dto.request.CategoryRequest;
+import ITProject.example.WebSelling.dto.request.ManufacturerRequest;
+import ITProject.example.WebSelling.entity.Category;
+import ITProject.example.WebSelling.entity.Manufacturer;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category addCategory(CategoryRequest categoryRequest);
+
+    Category getCategoryById(Long id);
+
+    List<Category> getAllCategories();
+
+    Category updateCategory(CategoryRequest categoryRequest, String name);
+
+    void deleteCategory(Long id);
+
+}
