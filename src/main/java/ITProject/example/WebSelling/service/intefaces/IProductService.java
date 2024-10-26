0e384e60ed29ebefb4f8b50cc73beb4b1b7bbd5f
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-10-27 02:12:55 +0700

    Chore: add uploadImage method

--- src/main/java/ITProject/example/WebSelling/service/intefaces/IProductService.java
+++ src/main/java/ITProject/example/WebSelling/service/intefaces/IProductService.java
@@ -3,6 +3,7 @@ package ITProject.example.WebSelling.service.intefaces;
 import ITProject.example.WebSelling.dto.request.ProductRequest;
 import ITProject.example.WebSelling.dto.response.ProductResponse;
 import ITProject.example.WebSelling.entity.Product;
+import ITProject.example.WebSelling.entity.ProductImage;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -20,5 +21,6 @@ public interface IProductService {
 
     ProductResponse getProduct(Long id);
 
+    List<ProductImage> uploadImages(Long productId, List<MultipartFile> files) throws IOException;
 
 }
