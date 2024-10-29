60e02826dbf10f58f36a11b7fab3ba7f142b6a33
Nguyễn Trường <nguyentruongpro19@gmail.com>
2024-10-29 10:19:44 +0700

    nothign

--- src/main/resources/db/migration/V1_create_specifications_table.sql
+++ src/main/resources/db/migration/V1_create_specifications_table.sql
@@ -7,4 +7,4 @@ CREATE TABLE `specifications`
     PRIMARY KEY (`spec_id`),
     KEY          `specifications_ibfk_1` (`product_id`),
     CONSTRAINT `specifications_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
